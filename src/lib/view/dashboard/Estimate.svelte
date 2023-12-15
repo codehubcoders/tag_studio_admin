299381334cd02e1eebeecaaff899915560050fcb
sisily11 <sisily11@naver.com>
2023-12-15 20:08:28 +0900

    대시보드

---
+++ src/lib/view/dashboard/Estimate.svelte
@@ -0,0 +1,156 @@
+<script>
+	import topSaleData from './dashboardChartContent.json';
+	const estimateData = topSaleData.dashBoard.estimate;
+	// let dataPeriod = myWorkData[myWork];
+	// export let dataPeriod;
+	export let extraStyle = 'selling-table-wrap';
+	export let defaultTable = 'table--default';
+</script>
+
+<div class="{extraStyle} table-responsive">
+	<table class="table {defaultTable} table-borderless">
+		<thead>
+			<tr>
+				<th>템플릿 이름</th>
+				<th>날짜</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each estimateData as data}
+				<tr>
+					<td>
+						<div class="selling-product-img d-flex align-items-center">
+							<span>{data.templateName}</span>
+						</div>
+					</td>
+					<td>{data.date}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
+
+<style lang="scss">
+	@import '../../../assets/sass/mixins/media-queries';
+	@import '../../../assets/sass/components/table';
+	@import '../../../assets/sass/mixins/functions';
+
+	:global{
+		.top-selling-product{
+			.card-body{
+				padding: 0 0 15px 0;
+			}
+		}
+	};
+	.selling-table-wrap {
+		.table--default tbody tr:last-child {
+			border-bottom: none;
+		}
+		.table--default tr th {
+			&:first-child {
+				padding-left: 15px;
+			}
+
+			&:last-child {
+				padding-right: 15px;
+			}
+		}
+
+		.table--default tr td {
+			&:first-child {
+				padding-left: 0;
+			}
+
+			&:last-child {
+				padding-right: 15px;
+			}
+		}
+		table {
+			thead {
+				tr {
+					background: var(--bg-normal);
+				}
+			}
+		}
+		@include lg {
+			padding-bottom: 0;
+		}
+
+		table {
+			tbody {
+				td {
+					white-space: nowrap;
+					font-size: 13px;
+					padding: 10px;
+					color: var(--color-dark);
+					text-align: center !important;
+					&:first-child {
+					text-align: left !important;
+					padding-left: 5px !important;
+				}
+				}
+
+				tr {
+					&:first-child {
+						td {
+							padding-top: 15px;
+						}
+					}
+				}
+			}
+
+			thead {
+				tr {
+					
+					th {
+						font-size: 13px;
+						font-weight: 500;
+						line-height: 18px;
+						color: var(--color-light);
+						text-transform: uppercase;
+						padding: 15px;
+						text-align: center !important;
+					}
+				}
+			}
+
+			tr th {
+				&:first-child {
+					border-radius: 6px 0 0 6px;
+					text-align: left !important;
+				}
+
+				&:last-child {
+					border-radius: 0 6px 6px 0;
+				}
+			}
+		}
+		.selling-product-img {
+			img {
+				margin-right: 10px;
+				width: 30px;
+				height: 30px;
+				border-radius: 5px;
+			}
+
+			span {
+				color: var(--color-dark);
+				font-size: 14px;
+				font-weight: 500;
+				line-height: lh(15px, 20px);
+				@include ssm {
+					padding-right: 15px;
+				}
+			}
+
+			&--2 {
+				img {
+					width: 25px;
+					height: 25px;
+					border-radius: 0;
+					object-fit: contain;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/(common-layout)/dashboard/+page.svelte
+++ src/routes/(common-layout)/dashboard/+page.svelte
@@ -9,6 +9,8 @@
 	// import TopSellingProduct from '../../../lib/view/dashboard/TopSellingProduct.svelte';
 	import MyWorkTable from '../../../lib/view/dashboard/MyWork.svelte';
 	import MyOderTable from '../../../lib/view/dashboard/MyOrder.svelte';
+	import EstimateTable from '../../../lib/view/dashboard/Estimate.svelte';
+	import CertificationTable from '../../../lib/view/dashboard/Certification.svelte';
 	// import BrowserState from '@view/dashboard/demoOne/BrowserState.svelte';
 
 	// const overviewDataSorted = overviewCardData.slice(0, 4);
@@ -46,15 +48,35 @@
 			</Col>
 
 		</Row>
-        		<!-- <Row>
+				<Row>
+
 			<Col xxl={6} lg={6} class="mb-25 mt-20">
-				<TopSellingProduct />
+				<p class="tableTitle">요청된 견적서</p>
+				<Card class="border-0 px-25 pt-20  top-selling-product">
+				<CardBody>
+					<EstimateTable />
+					</CardBody>
+				</Card>
 			</Col>
 
 			<Col xxl={6} lg={6} class="mb-25 mt-20">
-				<TopSellingProduct />
+			<p class="tableTitle">소속인증 요청</p>
+				<Card class="border-0 px-25 pt-20 top-selling-product">
+				<CardBody >
+					<CertificationTable />
+					</CardBody>
+				</Card>
 			</Col>
 
-		</Row> -->
+		</Row>
 	</Container>
 </div>
+<style lang="scss">
+.tableTitle{
+	font-size: 12px;
+	padding: 5px 10px;;
+	background-color: var(--color-primary);
+	border-radius: 6px;
+	color: white;
+}
+</style>
